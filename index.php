a41ace37665e6c5947389d07fafed76f20362969
Charles WANG <1225827513@qq.com>
2021-04-08 16:16:57 +0800

    Index Page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <form method="post">
+    <label>Email:</label>
+    <input type="text" name="email">
+    <br>
+    <label>Password:</label>
+    <input type="password" name="password">
+    <br>
+    <input type="submit" name="login" value="Login">
+    </form>
+    <?php
+    require_once './manager/UserManager.class.php';
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $user = SimpleUserManager::authenticate($_POST['email'], $_POST['password']);
+        if ($user != null) {
+            header('Location: post.php?email=' . $user->getEmail());
+            return;
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ manager/UserManager.class.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'C:\MAMP\htdocs\group_project_1\class\User.php';
+
+interface IUserManager {
+    public static function authenticate($emailInput, $passwordInput);
+}
+
+class SimpleUserManager implements IUserManager {
+    private static $email = 'test@test.com';
+    private static $password = '1234';
+
+    public static function authenticate($emailInput, $passwordInput) {
+        try {
+            if ($emailInput == self::$email && $passwordInput == self::$password) {
+                return new User($emailInput, $passwordInput);
+            }
+            else {
+                throw new Exception('Invalid email or password');
+            }
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
